63c3b4f756144c385a2fd30291d8807f7d108606
Aditya Subramanian <adu101992@gmail.com>
2026-09-08 13:41:59 +0530

    Games get highlighted when near them to signify that they can be collected!

---
+++ game-hoarder/Scripts/game_selection.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+@export var gameTex : Sprite2D
+@export var origColour : Color
+@export var currColour : Color
+
+@export var playerInRegion : bool
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	origColour = Color(1.0, 1.0, 1.0, 0.5)
+	currColour = Color(1.0, 1.0, 1.0, 2.0)
+	playerInRegion = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if playerInRegion:
+		gameTex.modulate = currColour
+	else:
+		gameTex.modulate = origColour
+
+func _on_body_entered(_body: Node2D) -> void:
+	if !_body.name.contains("Player"):
+		return
+
+	playerInRegion = true
+
+
+func _on_body_exited(_body: Node2D) -> void:
+	if !_body.name.contains("Player"):
+		return
+
+	playerInRegion = false
